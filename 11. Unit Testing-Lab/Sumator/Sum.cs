1af774383799c4cb9b9602019973bea15c36d82d
calisthenicsGuy <radevradoslav12@gmail.com>
2022-06-27 23:37:21 +0300

    unit testing lab

---
+++ 11. Unit Testing-Lab/Sumator/Sum.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Summator
+{
+    public class Sum
+    {
+        public long SumOfDigits(long num)
+        {
+            long sum = 0;
+            num = Math.Abs(num);
+            while (num != 0)
+            {
+                sum += num % 10;
+                num /= 10;
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ 11. Unit Testing-Lab/Summator.Test/SummatorTests.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Summator.Tests
+{
+    [TestFixture]
+    public class SummatorTests
+    {
+        [Test]
+        public void SumOfDigits_ShouldWorkCorrectlyWithADigit()
+        {
+            // Arrange
+            var summator = new Sum();
+            var givenNumber = 8;
+            var expectedResult = 8;
+
+            // Act
+            var result = summator.SumOfDigits(givenNumber);
+
+            // Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [Test]
+        public void SumOfDigits_ShouldWorkCorrectlyWithMilloins()
+        {
+            var summator = new Sum();
+            Assert.AreEqual(7, summator.SumOfDigits(1111111));
+        }
+
+        [Test]
+        public void SumOfDigits_ShouldWorkCorrectlyWithNegativeMillions()
+        {
+            var summator = new Sum();
+            Assert.AreEqual(7, summator.SumOfDigits(-1111111));
+        }
+
+        [Test]
+        public void SumOfDigits_ShouldWorkCorrectlyWithZero()
+        {
+            var summator = new Sum();
+            Assert.AreEqual(0, summator.SumOfDigits(0));
+        }
+    }
+}
