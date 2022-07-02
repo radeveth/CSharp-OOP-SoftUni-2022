930f6feeeffaf7b9affbb0d6aa7569f0c0240dc2
calisthenicsGuy <radevradoslav12@gmail.com>
2022-07-02 21:24:42 +0300

    oop exam tasks added

---
+++ Exams/OOP Exam 15 August 2021/CarRacing/Core/Controller.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+using CarRacing.Models.Maps;
+using CarRacing.Models.Cars;
+using CarRacing.Repositories;
+using CarRacing.Models.Racers;
+using CarRacing.Core.Contracts;
+using CarRacing.Utilities.Messages;
+using CarRacing.Models.Cars.Contracts;
+using CarRacing.Models.Maps.Contracts;
+using CarRacing.Models.Racers.Contracts;
+
+namespace CarRacing.Core
+{
+    public class Controller : IController
+    {
+        private CarRepository cars;
+        private RacerRepository racers;
+        private IMap map;
+
+        public Controller()
+        {
+            cars = new CarRepository();
+            racers = new RacerRepository();
+            map = new Map();
+        }
+
+        public string AddCar(string type, string make, string model, string VIN, int horsePower)
+        {
+            ICar newCar;
+            if (type == "SuperCar")
+            {
+                newCar = new SuperCar(make, model, VIN, horsePower);
+            }
+            else if (type == "TunedCar")
+            {
+                newCar = new TunedCar(make, model, VIN, horsePower);
+            }
+            else
+            {
+                throw new ArgumentException(ExceptionMessages.InvalidCarType);
+            }
+            
+            this.cars.Add(newCar);
+
+            return $"Successfully added car {newCar.Make} {newCar.Model} ({newCar.VIN}).";
+        }
+
+        public string AddRacer(string type, string username, string carVIN)
+        {
+            ICar targetCar = this.cars.FindBy(carVIN);
+            if (targetCar == null)
+            {
+                throw new ArgumentException(ExceptionMessages.CarCannotBeFound);
+            }
+
+            IRacer newRacer;
+            if (type == "ProfessionalRacer")
+            {
+                newRacer = new ProfessionalRacer(username, targetCar);
+            }
+            else if (type == "TunedCar")
+            {
+                newRacer = new StreetRacer(username, targetCar);
+            }
+            else
+            {
+                throw new ArgumentException(ExceptionMessages.InvalidRacerType);
+            }
+
+            this.racers.Add(newRacer);
+            return $"Successfully added racer {username}.";
+        }
+
+        public string BeginRace(string racerOneUsername, string racerTwoUsername)
+        {
+            IRacer racerOne = this.racers.FindBy(racerOneUsername);
+            if (racerOne == null)
+            {
+                throw new ArgumentException(
+                    String.Format(ExceptionMessages.RacerCannotBeFound, racerOneUsername));
+            }
+
+            IRacer racerTwo = this.racers.FindBy(racerTwoUsername);
+            if (racerTwo == null)
+            {
+                throw new ArgumentException(
+                    String.Format(ExceptionMessages.RacerCannotBeFound, racerTwoUsername));
+            }
+
+            return this.map.StartRace(racerOne, racerTwo);
+        }
+
+        public string Report()
+        {
+            var racers = this.racers.Models
+                .OrderByDescending(x => x.DrivingExperience).ThenBy(x => x.Username);
+
+
+            StringBuilder sb = new StringBuilder();
+            foreach (IRacer racer in racers)
+            {
+                sb.Append(racer.ToString());
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ Exams/OOP Exam 15 August 2021/CarRacing/Models/Maps/Map.cs
@@ -0,0 +1,62 @@
+﻿using CarRacing.Models.Maps.Contracts;
+using CarRacing.Models.Racers.Contracts;
+
+namespace CarRacing.Models.Maps
+{
+    public class Map : IMap
+    {
+        public string StartRace(IRacer racerOne, IRacer racerTwo)
+        {
+            if (!racerOne.IsAvailable())
+            {
+                return $"{racerTwo.Username} wins the race! {racerOne.Username} was not available to race!";
+            }
+            else if (!racerTwo.IsAvailable())
+            {
+                return $"{racerOne.Username} wins the race! {racerTwo.Username} was not available to race!";
+            }
+            else if (!racerOne.IsAvailable() && !racerTwo.IsAvailable())
+            {
+                return $"Race cannot be completed because both racers are not available!";
+            }
+
+            racerOne.Car.Drive();
+            racerTwo.Car.Drive();
+
+            double chancheForWinOfRacerOne = racerOne.Car.HorsePower * racerOne.DrivingExperience;
+            double chancheForWinOfRacerTwo = racerTwo.Car.HorsePower * racerTwo.DrivingExperience;
+            if (racerOne.RacingBehavior == "strict")
+            {
+                chancheForWinOfRacerOne *= 1.2;
+            }
+            else if (racerOne.RacingBehavior == "aggressive")
+            {
+                chancheForWinOfRacerOne *= 1.1;
+            }
+
+            if (racerTwo.RacingBehavior == "strict")
+            {
+                chancheForWinOfRacerTwo *= 1.2;
+            }
+            else if (racerTwo.RacingBehavior == "aggressive")
+            {
+                chancheForWinOfRacerTwo *= 1.1;
+            }
+
+            bool isRacerOneWin = chancheForWinOfRacerOne > chancheForWinOfRacerTwo;
+
+            if (isRacerOneWin)
+            {
+                return $"{racerOne.Username} has just raced against {racerTwo.Username}! " +
+                    $"{racerOne.Username} is the winner!";
+            }
+            else if (!isRacerOneWin)
+            {
+                return $"{racerOne.Username} has just raced against {racerTwo.Username}! " +
+                    $"{racerTwo.Username} is the winner!";
+            }
+
+            return null;
+        }
+    }
+}
